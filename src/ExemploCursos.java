b80ad2a9eda8b1ea22d762ec561583915d621fbd
Lucas Marchi <lucas-marchi@hotmail.com>
2018-02-04 12:49:10 -0200

    Mais streams apis e collections

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,24 +1,27 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Curso {
+	
     private String nome;
-    private int numeroDenumeroDealunos;
+    private int numeroDenumeroDeAlunos;
 
     public Curso(String nome, int numeroDealunos) {
         this.nome = nome;
-        this.numeroDenumeroDealunos = numeroDealunos;
+        this.numeroDenumeroDeAlunos = numeroDealunos;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public int getNumeroDealunos() {
-        return numeroDenumeroDealunos;
+    public int getNumeroDeAlunos() {
+        return numeroDenumeroDeAlunos;
     }
 }
+
 public class ExemploCursos {
 	
 	public static void main(String[] args) {
@@ -30,21 +33,42 @@ public class ExemploCursos {
 		cursos.add(new Curso("C", 55));
 		
 		//Ordenando pela quantidade de alunos usando method reference
-		cursos.sort(Comparator.comparing(Curso::getNumeroDealunos));
+		cursos.sort(Comparator.comparing(Curso::getNumeroDeAlunos));
 		
 		//Usando stream para filtrar por quantidade de alunos e printar na tela
 		cursos.stream()
-			.filter(curso -> curso.getNumeroDealunos() > 100)
+			.filter(curso -> curso.getNumeroDeAlunos() > 100)
 			.map(Curso::getNome)
 			.forEach(System.out::println);
 		
 		//Usando stream para filtrar por quantidade de alunos e somar
 		int soma = cursos.stream()
-			.filter(curso -> curso.getNumeroDealunos() > 100)
-			.mapToInt(Curso::getNumeroDealunos)
+			.filter(curso -> curso.getNumeroDeAlunos() > 100)
+			.mapToInt(Curso::getNumeroDeAlunos)
 			.sum();
 		
 		System.out.println(soma);
+		
+		//Usando stream para filtrar por quantidade de alunos, pegar o primeiro e caso ele exista pritar na tela
+		cursos.stream()
+		   .filter(c -> c.getNumeroDeAlunos() > 50)
+		   .findFirst()
+		   .ifPresent(curso -> System.out.println(curso.getNome()));
+		
+		//Usando stream para calcular a media de alunos por curso e caso não tenha problemas com a media printa na tela
+		cursos.stream()
+			.mapToInt(curso -> curso.getNumeroDeAlunos())
+			.average()
+			.ifPresent(System.out::println);
+		
+		//Usando stream para filtrar por quantidade de alunos converter para um map e iterar sobre o mesmo
+		cursos.stream()
+				.filter(curso -> curso.getNumeroDeAlunos() > 50)
+				.collect(Collectors.toMap(Curso::getNome, Curso::getNumeroDeAlunos))
+				.forEach((nome, numeroDeAlunos) -> System.out.println(nome + " " + numeroDeAlunos));
+		
+		
+
 		
 	}
 
